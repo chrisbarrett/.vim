e3fbd2813080099cd6d2be4acca84d482939ce93
Chris Barrett <chris.d.barrett@me.com>
2012-12-02 20:47:28 +1300

    reformat configure script

--- configure.rb
+++ configure.rb
@@ -28,12 +28,12 @@ require 'fileutils'
 bundles_dir = File.join File.dirname(__FILE__), 'bundle'
 FileUtils.cd bundles_dir
 
-puts '--> Deleting existing bundles...'
+puts '--> Deleting existing bundles'
 Dir['*'].each {|d| FileUtils.rm_rf d }
 
 git_repos.each do |url|
   dir = url.split('/').last.sub(/\.git$/, '')
-  puts "--> Downloading #{url}..."
+  puts "--> Downloading #{url}"
   `git clone #{url} #{dir}`
   FileUtils.rm_rf File.join(dir, '.git')
 end
