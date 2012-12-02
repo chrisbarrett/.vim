336b54849f2af47f9363c65ba696912c091e916a
Chris Barrett <chris.d.barrett@me.com>
2012-12-03 10:16:37 +1300

    fix mistyped git url, add more repos

--- configure.rb
+++ configure.rb
@@ -1,24 +1,23 @@
 #!/usr/bin/env ruby
 
-git_repos = [
+repos = [
   # ruby
   'git://github.com/vim-ruby/vim-ruby.git',
   'git://github.com/tpope/vim-endwise.git',
   # git
   'git://github.com/tpope/vim-git.git',
   'git://github.com/airblade/vim-rooter.git',
-  #languages
   'git://github.com/tpope/vim-fugitive.git',
+  # languages
   'git://github.com/tpope/vim-markdown.git',
-  'git://giub.com/ervandew/supertab.git',
-  'git://github.com/msanders/snipmate.vim.git',
+  # editing
+  'git://github.com/ervandew/supertab.git',
   'git://github.com/tsaleh/vim-matchit.git',
   'git://github.com/tpope/vim-repeat.git',
   'git://github.com/tpope/vim-surround.git',
-  'git://github.com/tsaleh/vim-tcomment.git',
+  'git://github.com/vim-scripts/tComment.git',
+  'git://github.com/vim-scripts/Auto-Pairs.git',
   'git://github.com/tpope/vim-ragtag.git',
-  'git://github.com/garbas/vim-snipmate.git',
-  'git://github.com/honza/snipmate-snippets.git',
   'git://github.com/shemerey/vim-peepopen.git',
   'git://github.com/tpope/vim-unimpaired.git'
 ]
@@ -31,7 +30,7 @@ FileUtils.cd bundles_dir
 puts '--> Deleting existing bundles'
 Dir['*'].each {|d| FileUtils.rm_rf d }
 
-git_repos.each do |url|
+repos.each do |url|
   dir = url.split('/').last.sub(/\.git$/, '')
   puts "--> Downloading #{url}"
   `git clone #{url} #{dir}`
